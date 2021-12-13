9019bfd1aefe7a615dbe14417b04bbeb5c78be45
Eomy <hmu1212@hotmail.com>
2021-12-13 22:28:52 +0900

    feat: implement contoroller

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,13 +1,26 @@
-const recipeContainer = document.querySelector('.recipe');
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    await model.loadRecipe(id);
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+    console.error(err);
+  }
 };
 
-// https://forkify-api.herokuapp.com/v2
-
-///////////////////////////////////////
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
