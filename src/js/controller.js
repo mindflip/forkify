6130851b2ade5fd7a4a0ddf6ff0fad719160e314
Eomy <hmu1212@hotmail.com>
2021-12-15 16:05:30 +0900

    feat: imeplement controlServings

--- src/js/controller.js
+++ src/js/controller.js
@@ -48,8 +48,17 @@ const controlPagination = function (goToPage) {
   paginationView.render(model.state.search);
 };
 
+const controlServings = function (newServings) {
+  // Update state of the recipe servings
+  model.updateServings(newServings);
+
+  // Update the recipe view
+  recipeView.render(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
 };
