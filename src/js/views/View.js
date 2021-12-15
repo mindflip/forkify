12e74faa3bce1b75657cf7f1800c670965d8d3ca
Eomy <hmu1212@hotmail.com>
2021-12-15 13:12:40 +0900

    fix: add search result render error

--- src/js/views/View.js
+++ src/js/views/View.js
@@ -5,7 +5,8 @@ export default class View {
   _data;
 
   render(data, render = true) {
-    if (!data || (Array.isArray(data) && data.length === 0)) return;
+    if (!data || (Array.isArray(data) && data.length === 0))
+      return this.renderError();
 
     this._data = data;
     const markup = this._generateMarkup();
