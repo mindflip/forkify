2e5ab1dc3c046d2541c165222b2d4d888d1e37d5
Eomy <hmu1212@hotmail.com>
2021-12-18 10:57:47 +0900

    feat: add config settings

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
-export const KEY = '<MY_KEY>';
+export const KEY = 'f9a68cc1-a413-4089-baec-d3fd9d8a8e0f';
+export const RES_PER_PAGE = '10';
+export const MODAL_CLOSE_SEC = 2.5;
